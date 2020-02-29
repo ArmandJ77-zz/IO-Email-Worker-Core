cd59c28912addd5069ea76629d03856fe3de81ce
armand.jordaan@leadhome.co.za <armand.jordaan@leadhome.co.za>
2020-02-29 18:54:40 +0200

    Update dataPropertyModel.ts

--- src/models/dataPropertyModel.ts
+++ src/models/dataPropertyModel.ts
@@ -1,6 +1,7 @@
 import ValidationModel from "./validationModel";
+import DateValidationModel from "./dateValidationmodel";
 
 export default interface DataPropertyModel {
   Name: string;
-  Validation: ValidationModel;
+  Validation: ValidationModel | DateValidationModel;
 }
