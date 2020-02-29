aaaafab9fa3fdf4003981c4161e3157427140bed
armand.jordaan@leadhome.co.za <armand.jordaan@leadhome.co.za>
2020-02-29 18:54:52 +0200

    WIP - validators

--- src/valueValidationHandler.ts
+++ src/valueValidationHandler.ts
@@ -1,7 +1,10 @@
 import DataProperty from "./models/dataPropertyModel";
 import ValidationModel from "./models/validationModel";
+import DateValidationModel from "./models/dateValidationmodel";
 import ValidationResponse from "./responses/validationResponse";
-import {ValidationTypes} from "./enums/validationTypes";
+import { ValidationTypes } from "./enums/validationTypes";
+import moment from "moment";
+
 export default class ValueValidation {
   constructor() {}
 
@@ -17,7 +20,15 @@ export default class ValueValidation {
           inputDataProps[idp][dataTemplateProps[dtpi].Name];
         const inputDataValidator = dataTemplateProps[dtpi].Validation;
 
-        let response = this.Validate(idp, inputDataValue, inputDataValidator);
+        let response = this.Validate(
+          dtpi,
+          inputDataValue,
+          dataTemplateProps[dtpi].Name,
+          inputDataValidator
+        );
+        // console.log(response);
+        result.push(response);
+        // console.log(response);
       }
     }
 
@@ -25,27 +36,124 @@ export default class ValueValidation {
 
     // console.log(dataPropertiesModel);
     // console.log(inputDataModel);
-
+    console.log("iteration done: " + result.length);
     return result;
   }
 
   Validate(
     inputDataIndex: number,
     value: any,
-    validationRule: ValidationModel
+    key: string,
+    validationRule: ValidationModel | DateValidationModel
   ): ValidationResponse {
-    const isEmptyString = val => !val || /^\s*$/.test(val);
-
     if (validationRule.ValueRequired) {
-      const isNotEmptyStringResult = isEmptyString(value);
-      return new ValidationResponse(inputDataIndex, false, this.buildValidationMessage());
-      console.log(value + " is empty " + isNotEmptyStringResult);
+      const isInvalidStringResult = this.isInvalidString(value);
+      if (isInvalidStringResult) {
+        return new ValidationResponse(
+          inputDataIndex,
+          false,
+          this.buildValidationMessage(
+            ValidationTypes.valueRequired,
+            inputDataIndex,
+            key,
+            value
+          )
+        );
+      }
     }
 
-    return 
+    switch (validationRule.Type) {
+      case "email":
+        if (this.isEmailValid(value) === false)
+          return new ValidationResponse(
+            inputDataIndex,
+            false,
+            this.buildValidationMessage(
+              ValidationTypes.email,
+              inputDataIndex,
+              key,
+              value
+            )
+          );
+        break;
+        // case "string":
+        //   if (!this.isStringValid(value))
+        //     return new ValidationResponse(
+        //       inputDataIndex,
+        //       false,
+        //       this.buildValidationMessage(
+        //         ValidationTypes.string,
+        //         inputDataIndex,
+        //         key,
+        //         value
+        //       )
+        //     );
+        //   break;
+        // case "number":
+        //   if (!this.isNumberValid(value))
+        //     return new ValidationResponse(
+        //       inputDataIndex,
+        //       false,
+        //       this.buildValidationMessage(
+        //         ValidationTypes.number,
+        //         inputDataIndex,
+        //         key,
+        //         value
+        //       )
+        //     );
+        //   break;
+        // case "bool":
+        //   if (!this.isBooleanValid(value))
+        //     return new ValidationResponse(
+        //       inputDataIndex,
+        //       false,
+        //       this.buildValidationMessage(
+        //         ValidationTypes.bool,
+        //         inputDataIndex,
+        //         key,
+        //         value
+        //       )
+        //     );
+        //   break;
+        // case "date":
+        //   if (
+        //     !this.isDateValid(
+        //       value,
+        //       (validationRule as DateValidationModel).format
+        //     )
+        //   )
+        //     return new ValidationResponse(
+        //       inputDataIndex,
+        //       false,
+        //       this.buildValidationMessage(
+        //         ValidationTypes.date,
+        //         inputDataIndex,
+        //         key,
+        //         value
+        //       )
+        //     );
+        break;
+    }
+
+    return new ValidationResponse(inputDataIndex, true, "");
   }
 
-  private buildValidationMessage = (validationType:, nodeIndex, onKey, withValueOf) =>
-  `${validationType} check failed on node ${nodeIndex}: ${onKey} value ${withValueOf}`;
+  private buildValidationMessage = (
+    validationType: ValidationTypes,
+    nodeIndex: number,
+    onKey: string,
+    withValueOf: any
+  ) =>
+    `${validationType} check failed on node ${nodeIndex}: ${onKey} value ${withValueOf}`;
 
+  private isInvalidString = val => /^\s*$|null|NULL/.test(val);
+  private isNumberValid = val =>
+    !val || /^(?!-0(\.0+)?$)-?(0|[1-9]\d*)(\.\d+)?$/.test(val);
+  private isBooleanValid = val => !val || /^(true|false|1|0)$/.test(val);
+  private isDateValid = (val, format) => moment(val, format).isValid();
+  private isEmailValid = val =>
+    !val ||
+    /^((([a-z]|d|[!#$%&'*+-/=?^_`{|}~]|[u00A0-uD7FFuF900-uFDCFuFDF0-uFFEF])+(.([a-z]|d|[!#$%&'*+-/=?^_`{|}~]|[u00A0-uD7FFuF900-uFDCFuFDF0-uFFEF])+)*)|((x22)((((x20|x09)*(x0dx0a))?(x20|x09)+)?(([x01-x08x0bx0cx0e-x1fx7f]|x21|[x23-x5b]|[x5d-x7e]|[u00A0-uD7FFuF900-uFDCFuFDF0-uFFEF])|(([x01-x09x0bx0cx0d-x7f]|[u00A0-uD7FFuF900-uFDCFuFDF0-uFFEF]))))*(((x20|x09)*(x0dx0a))?(x20|x09)+)?(x22)))@((([a-z]|d|[u00A0-uD7FFuF900-uFDCFuFDF0-uFFEF])|(([a-z]|d|[u00A0-uD7FFuF900-uFDCFuFDF0-uFFEF])([a-z]|d|-||_|~|[u00A0-uD7FFuF900-uFDCFuFDF0-uFFEF])*([a-z]|d|[u00A0-uD7FFuF900-uFDCFuFDF0-uFFEF]))).)+(([a-z]|[u00A0-uD7FFuF900-uFDCFuFDF0-uFFEF])+|(([a-z]|[u00A0-uD7FFuF900-uFDCFuFDF0-uFFEF])+([a-z]+|d|-|.{0,1}|_|~|[u00A0-uD7FFuF900-uFDCFuFDF0-uFFEF])?([a-z]|[u00A0-uD7FFuF900-uFDCFuFDF0-uFFEF])))$/.test(
+      val
+    );
 }
--- test/valueValidationHandler.spec.ts
+++ test/valueValidationHandler.spec.ts
@@ -7,7 +7,8 @@ import {
   GivenAValidDataSet,
   GivenAnInValidDataSetWithMissingProperties,
   GivenAnInValidDataSetWithIncorrectlyNamedProperties,
-  GivenAnInValidDataSetWithMissingInputDataValues
+  GivenAnInValidDataSetWithMissingInputDataValues,
+  GivenAnInValidDataSetWithIncorrectEmailValues
 } from "./testData/Data";
 
 const expect = chai.expect;
@@ -15,8 +16,8 @@ const expect = chai.expect;
 const sut = "ValueValidation";
 
 describe(sut, function() {
-  describe("Given An InValid DataSet With Missing Input Data Values", function() {
-    it("should return a list of validation failed responses", function() {
+  describe("Given An InValid DataSet", function() {
+    it("should return a list of validation failed responses where is required = true", function() {
       let leDataSet = GivenAnInValidDataSetWithMissingInputDataValues;
 
       let inputData = <InputDataModel>(<unknown>leDataSet.data.InputData);
@@ -31,11 +32,36 @@ describe(sut, function() {
         dataProperties.Properties,
         inputData.Data
       );
+      console.log(results.length);
 
-      // results.forEach(result => {
-      //   expect(result.IsValid).to.equal(false);
+      results.forEach(result => {
+        expect(result.IsValid).to.equal(false);
+      });
+    });
 
-      // });
+    it("should return a list of validation failed responses where emails aren't valid", function() {
+      let leDataSet = GivenAnInValidDataSetWithIncorrectEmailValues;
+
+      let inputData = <InputDataModel>(<unknown>leDataSet.data.InputData);
+      let dataProperties = <DataPropertiesModel>(
+        (<unknown>leDataSet.data.DataTemplate)
+      );
+
+      let handler = new ValueValidationHandler();
+
+      let results = handler.ValidateValues(
+        dataProperties.Properties,
+        inputData.Data
+      );
+
+      console.log(results.length);
+
+      const testResults = results.filter(x => x.IsValid === false);
+      expect(testResults.length).to.equal(4);
+
+      testResults.forEach(result => {
+        expect(result.Error).to.contain("email check failed");
+      });
     });
   });
 });
