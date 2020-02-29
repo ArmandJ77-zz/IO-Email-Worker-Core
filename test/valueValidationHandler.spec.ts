cf8da7e1c3c92a754e6b2ed64109cd45a5115a18
armand.jordaan@leadhome.co.za <armand.jordaan@leadhome.co.za>
2020-02-29 19:58:32 +0200

    Update valueValidationHandler.spec.ts

--- test/valueValidationHandler.spec.ts
+++ test/valueValidationHandler.spec.ts
@@ -4,13 +4,11 @@ import InputDataModel from "../src/models/inputDataModel";
 import * as chai from "chai";
 import "mocha";
 import {
-  GivenAValidDataSet,
-  GivenAnInValidDataSetWithMissingProperties,
-  GivenAnInValidDataSetWithIncorrectlyNamedProperties,
   GivenAnInValidDataSetWithMissingInputDataValues,
   GivenAnInValidDataSetWithIncorrectEmailValues,
   GivenAnInValidDataSetWithIncorrectStringValues,
-  GivenAnInValidDataSetWithIncorrectNumericsValues
+  GivenAnInValidDataSetWithIncorrectNumericsValues,
+  GivenAnInValidDataSetWithIncorrectBooleanValues
 } from "./testData/Data";
 
 const expect = chai.expect;
@@ -19,7 +17,7 @@ const sut = "ValueValidation";
 
 describe(sut, function() {
   describe("Given An InValid DataSet", function() {
-    it("should return a list of validation failed responses where is required = true", function() {
+    it("should return a list of validation failed responses where required = true", function() {
       let leDataSet = GivenAnInValidDataSetWithMissingInputDataValues;
 
       let inputData = <InputDataModel>(<unknown>leDataSet.data.InputData);
@@ -103,13 +101,33 @@ describe(sut, function() {
         inputData.Data
       );
 
-      // console.log(results);
       const testResults = results.filter(x => x.IsValid === false);
-      console.log(testResults);
-
       expect(testResults.length).to.equal(2);
       testResults.forEach(result => {
         expect(result.Error).to.contain("number check failed");
+      });
+    });
+
+    it("should return a list of validation failed responses where boolean aren't valid", function() {
+      let leDataSet = GivenAnInValidDataSetWithIncorrectBooleanValues;
+
+      let inputData = <InputDataModel>(<unknown>leDataSet.data.InputData);
+      let dataProperties = <DataPropertiesModel>(
+        (<unknown>leDataSet.data.DataTemplate)
+      );
+
+      let handler = new ValueValidationHandler();
+
+      let results = handler.ValidateValues(
+        dataProperties.Properties,
+        inputData.Data
+      );
+
+      const testResults = results.filter(x => x.IsValid === false);
+
+      expect(testResults.length).to.equal(1);
+      testResults.forEach(result => {
+        expect(result.Error).to.contain("boolean check failed");
       });
     });
   });
