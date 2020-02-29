efb20b4e181cfa13808325a873e7ba18bd12753e
armand.jordaan@leadhome.co.za <armand.jordaan@leadhome.co.za>
2020-02-29 23:45:24 +0200

    Create dataInjectionHandler.ts

---
+++ src/dataInjectionHandler.ts
@@ -0,0 +1,10 @@
+import handlebars from "handlebars";
+import DataPropertiesModel from "./models/dataPropertiesModel";
+import InputDataModel from "./models/inputDataModel";
+
+export default class DataInjectionHandler {
+  InjectValues(
+    dataPropertiesModel: DataPropertiesModel,
+    inputDataModel: InputDataModel
+  ): void {}
+}
