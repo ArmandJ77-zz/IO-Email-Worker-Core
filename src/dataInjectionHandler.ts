7c9875c64b43e3989598af4099de3caf131a4341
armand.jordaan@leadhome.co.za <armand.jordaan@leadhome.co.za>
2020-03-01 17:19:54 +0200

    Update dataInjectionHandler.ts

--- src/dataInjectionHandler.ts
+++ src/dataInjectionHandler.ts
@@ -1,10 +1,49 @@
 import handlebars from "handlebars";
-import DataPropertiesModel from "./models/dataPropertiesModel";
 import InputDataModel from "./models/inputDataModel";
+import DataInjectionErrorModel from "./models/DataInjectionErrorModel";
+import DataInjectionSuccessModel from "./models/dataInjectionSuccessModel";
+import DataInjectionResponse from "./responses/dataInjectionResponse";
 
 export default class DataInjectionHandler {
+  constructor() {}
+
   InjectValues(
-    dataPropertiesModel: DataPropertiesModel,
+    emailTemplate: any,
     inputDataModel: InputDataModel
-  ): void {}
+  ): DataInjectionResponse {
+    let successModels: Array<DataInjectionSuccessModel> = [];
+    let errorModels: Array<DataInjectionErrorModel> = [];
+
+    for (let i = 0; i < inputDataModel.Data.length; i++) {
+      try {
+        const hbsTemplate = handlebars.compile(emailTemplate);
+        const enrichedTemplate = hbsTemplate(inputDataModel.Data[i]);
+
+        const successModel = new DataInjectionSuccessModel(
+          i,
+          inputDataModel.Data[i]["Email"],
+          enrichedTemplate
+        );
+
+        successModels.push(successModel);
+      } catch (ex) {
+        let errorModel = new DataInjectionErrorModel(
+          i,
+          inputDataModel.Data[i],
+          emailTemplate,
+          ex
+        );
+
+        errorModels.push(errorModel);
+      }
+    }
+
+    const response = new DataInjectionResponse(
+      errorModels.length > 0,
+      errorModels,
+      successModels
+    );
+
+    return response;
+  }
 }
