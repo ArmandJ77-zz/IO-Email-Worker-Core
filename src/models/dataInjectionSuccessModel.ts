d5ca36358aec168e767524f16d5034b3466feb52
armand.jordaan@leadhome.co.za <armand.jordaan@leadhome.co.za>
2020-03-01 17:19:12 +0200

    Create dataInjectionSuccessModel.ts

---
+++ src/models/dataInjectionSuccessModel.ts
@@ -0,0 +1,22 @@
+export default class DataInjectionSuccessModel {
+  private _inputDataIndex: number;
+  public InputDataIndex(): number {
+    return this._inputDataIndex;
+  }
+
+  private _to: string;
+  public To(): string {
+    return this._to;
+  }
+
+  private _email: string;
+  public Email(): string {
+    return this._email;
+  }
+
+  constructor(inputDataIndex: number, to: string, email: string) {
+    this._inputDataIndex = inputDataIndex;
+    this._to = to;
+    this._email = email;
+  }
+}
