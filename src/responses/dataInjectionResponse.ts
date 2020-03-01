1b854b1a3c6871c4d03d6e6e025a31837d43a037
armand.jordaan@leadhome.co.za <armand.jordaan@leadhome.co.za>
2020-03-01 18:44:52 +0200

    Update dataInjectionResponse.ts

--- src/responses/dataInjectionResponse.ts
+++ src/responses/dataInjectionResponse.ts
@@ -2,28 +2,9 @@ import DataInjectionErrorModel from "../models/dataInjectionErrorModel";
 import DataInjectionSuccessModel from "../models/dataInjectionSuccessModel";
 
 export default class DataInjectionResponse {
-  private _isSuccess: Boolean;
-  public IsSuccess(): Boolean {
-    return this._isSuccess;
-  }
+  IsSuccess: Boolean;
+  ErrorModels: Array<DataInjectionErrorModel> = [];
+  SuccessModels: Array<DataInjectionSuccessModel> = [];
 
-  private _errorModels: Array<DataInjectionErrorModel>;
-  public ErrorModels(): Array<DataInjectionErrorModel> {
-    return this._errorModels;
-  }
-
-  private _successModels: Array<DataInjectionSuccessModel>;
-  public SuccessModels(): Array<DataInjectionSuccessModel> {
-    return this._successModels;
-  }
-
-  constructor(
-    isSuccess: boolean,
-    errorModels: Array<DataInjectionErrorModel>,
-    successModels: Array<DataInjectionSuccessModel>
-  ) {
-    this._isSuccess = isSuccess;
-    this._errorModels = errorModels;
-    this._successModels = successModels;
-  }
+  constructor() {}
 }
