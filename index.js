e44f9a0ec93e77c68099ec6be106e441aaff2b2a
armandjordaan@live.co.za <armandjordaan@live.co.za>
2020-03-07 12:35:04 +0200

    Clean up branch

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("hallo");
-class TestMessage {
-    constructor() { }
-}
-exports.default = TestMessage;
--- src/distributionHandler.ts
+++
@@ -1,5 +0,0 @@
-export default class DistributionHandler {
-  constructor() {}
-
-  Distribute(): void {}
-}
